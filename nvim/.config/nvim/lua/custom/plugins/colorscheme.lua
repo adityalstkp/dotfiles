523664b3f9727979c2ccdfc17cf7cc6546623d0a
Aditya Septiadi <aditya.septiadi@bytedance.com>
2025-05-10 14:06:26 +0700

    chore: dev update

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require('kanso').setup {
+                transparent = true
+            }
             vim.cmd('colorscheme kanso-zen')
         end,
     },
