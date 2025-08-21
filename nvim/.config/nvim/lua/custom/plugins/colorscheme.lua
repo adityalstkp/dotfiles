0a2087b74af8e07854991b4c365a65004966b3db
Aditya Lucky Septiadi <54019824+adityalstkp@users.noreply.github.com>
2025-08-22 04:58:39 +0700

    chore: update some plugs and lsp (#11)
    * chore: dev update
    
    * chore: bump and fix lsp
    
    * chore: rmv unsused color
    
    * chore: turn off blur
    
    * chore: treesitter ctx toggle cmd
    
    * chore: update to 0.11.3 and all deps
    
    * chore: enable mouse look
    
    * fix: gopls conf

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -4,59 +4,10 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require('kanso').setup {
+                transparent = true
+            }
             vim.cmd('colorscheme kanso-zen')
-        end,
-    },
-    {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        enabled = false,
-        config = function()
-            require('rose-pine').setup {
-                styles = {
-                    bold = true,
-                    italic = false,
-                    transparency = false,
-                },
-            }
-            vim.cmd 'colorscheme rose-pine-moon'
-        end,
-    },
-    {
-        'aliqyan-21/darkvoid.nvim',
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        opts = {},
-        config = function()
-            local glow_base_color = '#1bfd9c'
-
-            require('darkvoid').setup {
-                transparent = true,
-                glow = true,
-                colors = {
-                    fg = '#c0c0c0',
-                    bg = '#1c1c1c',
-                    cursor = '#bdfe58',
-                    line_nr = '#404040',
-                    visual = '#303030',
-                    comment = '#585858',
-                    string = '#d1d1d1',
-                    func = '#e1e1e1',
-                    kw = '#f1f1f1',
-                    identifier = '#b1b1b1',
-                    type = '#a1a1a1',
-                    type_builtin = '#c5c5c5', -- current
-                    -- type_builtin = '#8cf8f7', -- glowy blue old (was present by default before type_builtin was introduced added here for people who may like it)
-                    search_highlight = glow_base_color,
-                    operator = glow_base_color,
-                    bracket = glow_base_color,
-                    preprocessor = '#4b8902',
-                    bool = '#66b2b2',
-                    constant = '#b2d8d8',
-                },
-            }
-            vim.cmd 'colorscheme darkvoid'
         end,
     }
 }
--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,9 +2,25 @@ return {
   -- bigfile handle
   {
     'LunarVim/bigfile.nvim',
+    config = function()
+      require("bigfile").setup {
+        filesize = 1,      -- size of the file in MiB, the plugin round file sizes to the closest MiB
+        pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
+        features = {       -- features to disable
+          "indent_blankline",
+          "illuminate",
+          "lsp",
+          "treesitter",
+          "syntax",
+          "matchparen",
+          "vimopts",
+          "filetype",
+        }
+      }
+    end
   },
   -- Detect tabstop and shiftwidth automatically
-  { 'tpope/vim-sleuth', event = 'VeryLazy' },
+  { 'tpope/vim-sleuth',      event = 'VeryLazy' },
   -- Add indentation guides even on blank lines
   {
     'lukas-reineke/indent-blankline.nvim',
