b90c161ae44d4b58b6beab38cf40835f57457684
Aditya Lucky Septiadi <54019824+adityalstkp@users.noreply.github.com>
2026-06-17 18:06:59 +0700

    chore: experiment some things (#16)
    * feat: omniwm and simplify things
    
    * chore: omniwm
    
    * chore: rmv omniwm
    
    * chore: back to aerospace
    
    * chore: try omniwm again
    
    * chore: vercel font
    
    * chore: omniwm corrupt
    
    * chore: omniwm sucks
    
    * chore: float rmv colorscheme
    
    * feat: sketchybar
    
    * chore: simple
    
    * chore: DRY

--- docs/alacritty.md
+++ docs/alacritty.md
@@ -2,3 +2,4 @@
 
 For MacOS can install with brew:
 1. font-jetbrains-mono-nerd-font
+2. font-geist-mono-nerd-font
--- nvim/.config/nvim/plugin/colorscheme.lua
+++ nvim/.config/nvim/plugin/colorscheme.lua
@@ -1,4 +1,3 @@
 vim.cmd 'colorscheme habamax'
 
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
